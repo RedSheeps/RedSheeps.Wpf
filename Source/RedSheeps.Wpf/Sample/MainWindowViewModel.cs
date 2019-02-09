a9f048cef40e1bba7a698ee24384c347c690c58e
Atsushi Nakamura <nuits.jp@live.jp>
2019-02-09 16:03:44 +0900

    Add Sample Project.

---
+++ Source/RedSheeps.Wpf/Sample/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RedSheeps.Wpf.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sample
+{
+    public class MainWindowViewModel
+    {
+        public ICommand InitializeCommand => new Command(OnInitialize);
+
+        private void OnInitialize()
+        {
+            
+        }
+    }
+}
