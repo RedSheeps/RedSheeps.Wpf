c5888bfd4cb05993e07bb18a2dde4c6219aab4eb
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2019-02-10 16:03:40 +0900

    Add CloseWindowAction.

---
+++ Source/RedSheeps.Wpf/RedSheeps.Wpf/Interactivity/CloseWindowAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace RedSheeps.Wpf.Interactivity
+{
+    public class CloseWindowAction : TriggerAction<DependencyObject>
+    {
+        protected override void Invoke(object parameter)
+        {
+            Window.GetWindow(AssociatedObject)?.Close();
+        }
+    }
+}
